3c49fe0e065d8bd7afdcb790a317b6b2a9f65d6a
LunaticCaptain127 <murvmarcos483@gmail.com>
2025-05-12 23:13:12 -0600

    Se agregó: actualizar.php, insertar.php, update.php y delete.php

--- index.php
+++ index.php
@@ -11,14 +11,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tutorial PHP</title>
+    <title>Tutorial-1 PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-5">
+    <div class="container mt-2">
+        <div class="row">
+            <h1 class="text-center">Proyecto CRUD</h1>
+        </div>
         <div class="row">
             <div class="col-md-3">
-                <h1>Ingresa los Datos del Alumno</h1>
+                <h3>Ingresa los Datos</h3>
                 <form action="insertar.php" method="post">
                     <input type="text"
                            name="ine"
@@ -27,17 +30,22 @@
                     <input type="text"
                            name="nombre"
                            class="form-control mb-3"
-                           placeholder="Escribe tu nombre">
+                           placeholder="Escribe tu Nombre">
                     <input type="text"
                            name="apellidos"
                            class="form-control mb-3"
-                           placeholder="Escribe tus apellidos">
+                           placeholder="Escribe tus Apellidos">
                     <input type="submit"
                            value="Insertar"
-                           class="btn btn_primary">
+                           class="btn btn-primary">
                 </form>
             </div>
             <div class="col-md-8">
+                <div class="row">
+                    <h3 class="text-center">
+                        Alumnos del Sistema
+                    </h3>
+                </div>
                 <table class="table">
                     <thead class="table-success table-striped">
                         <tr>
@@ -52,7 +60,7 @@
                     </thead>
                     <tbody>
                         <?php
-                            while($row = mysqli_fetch_array($query)){
+                            while($row=mysqli_fetch_array($query)){
                         ?>
                             <tr>
                                 <td>
@@ -68,9 +76,11 @@
                                     <?php echo $row['apellidos'] ?>
                                 </td>
                                 <td>
-                                    <a href="" class="btn btn-warning">Editar</a>
+                                    <a href="actualizar.php?id=<?php echo $row['cod_estudiante']?>"
+                                        class="btn btn-warning">Editar</a>
                                     |
-                                    <a href="" class="btn btn-danger">Borrar</a>
+                                    <a href="delete.php?id=<?php echo $row['cod_estudiante']?>" 
+                                        class="btn btn-danger">Borrar</a>
                                 </td>
                             </tr>
                         <?php
