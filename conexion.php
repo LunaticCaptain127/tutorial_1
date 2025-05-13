679e65934b4a11247bd59a7dad914188de1b6e25
LunaticCaptain127 <murvmarcos483@gmail.com>
2025-05-12 20:51:51 -0600

    Se creo conecion.php

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+    function conectar() {
+        $host = "localhost:8889";
+        $user = "root";
+        $pass = "root";
+        $bd = "tutorial_1";
+        $conn = mysqli_connect($host, $user, $pass);
+        mysqli_select_db($bd);
+        return $conn;
+    }
+?>
